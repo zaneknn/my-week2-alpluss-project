a461d7cb57e5a47d6df6da17161a8fe2c11f201e
Zane Jakuseva <zane.knn@gmail.com>
2024-02-02 22:53:54 +0100

    Upgraded UX.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function displayAnswer(response) {
-  let answer = document.querySelector("#course");
-  answer.innerHTML = response.data.answer;
-
   new Typewriter("#course", {
     strings: response.data.answer,
     autoStart: true,
@@ -12,16 +9,17 @@ function displayAnswer(response) {
 
 function generateCourses(event) {
   event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "ta8d0cd5f43b6fe4f1cd0019401obcd1";
   let context =
-    "Your are a amrt AI Assistant that knows the best available online programming courses. The list of courses must be provided in HTML format. Example: <li>Course</li>";
-  let prompt =
-    "Tell me the most popular online programming courses including links.Maximum 11 listed courses. Sign the list with `This is just a beginning.And it's only going to get better from here.` in <strong> element. ";
+    "Your are a smart AI Assistant that knows the best available online programming courses. The list of courses must be provided in HTML format.Maximum 5 listed courses. Sign the list with `This is just a beginning.And it's only going to get better from here.` in <strong> element. Include links.";
+  let prompt = `Tell me the most popular online programming courses about  ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let courseElement = document.querySelector("#course");
+  courseElement.classList.remove("hidden");
   courseElement.innerHTML = "Generating courses for you.. please wait 👩🏼‍💻";
-  courseElement.style.display = `block`;
-  console.log("called the AI api");
+
   axios.get(apiUrl).then(displayAnswer);
 }
 
