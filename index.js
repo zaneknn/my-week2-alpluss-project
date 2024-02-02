697336e572ddbee0bb734d900ab8a4fc75e59d20
Zane Jakuseva <zane.knn@gmail.com>
2024-02-02 22:04:24 +0100

    Added AI Api integration.

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function generateCourses(event) {
-  event.preventDefault();
+function displayAnswer(response) {
+  let answer = document.querySelector("#course");
+  answer.innerHTML = response.data.answer;
 
   new Typewriter("#course", {
-    strings: "The list of courses will be listed here:",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 0.5,
-    cursor: "",
+    delay: 20,
+    cursor: null,
   });
+}
+
+function generateCourses(event) {
+  event.preventDefault();
+  let apiKey = "ta8d0cd5f43b6fe4f1cd0019401obcd1";
+  let context =
+    "Your are a amrt AI Assistant that knows the best available programming courses online. The list of courses must be provided in HTML format. Example: <li>Course</li>";
+  let prompt =
+    "Tell me the most popular online programming courses.Maximum 11 listed courses. Sign the list with `This is just a beginning.And it's only going to get better from here.` in <strong> element. </strong> ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let courseElement = document.querySelector("#course");
+  courseElement.innerHTML = "Generating courses for you.. please wait";
+
+  console.log("called the AI api");
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let programmingCourseElement = document.querySelector("#course-generator-form");
