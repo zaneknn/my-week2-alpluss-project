99d26ad86b0acd23f9be3cd9581a44f6acb22c6f
Zane Jakuseva <zane.knn@gmail.com>
2024-02-02 22:07:52 +0100

    Upgraded AI.

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function generateCourses(event) {
   event.preventDefault();
   let apiKey = "ta8d0cd5f43b6fe4f1cd0019401obcd1";
   let context =
-    "Your are a amrt AI Assistant that knows the best available programming courses online. The list of courses must be provided in HTML format. Example: <li>Course</li>";
+    "Your are a amrt AI Assistant that knows the best available online programming courses. The list of courses must be provided in HTML format. Example: <li>Course</li>";
   let prompt =
-    "Tell me the most popular online programming courses.Maximum 11 listed courses. Sign the list with `This is just a beginning.And it's only going to get better from here.` in <strong> element. </strong> ";
+    "Tell me the most popular online programming courses including links.Maximum 11 listed courses. Sign the list with `This is just a beginning.And it's only going to get better from here.` in <strong> element. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let courseElement = document.querySelector("#course");
   courseElement.innerHTML = "Generating courses for you.. please wait";
